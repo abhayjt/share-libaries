390abfa2f3a3101a876bf24c4781c0060115302d
abhayjt <ad.dubey23@gmail.com>
2025-06-04 12:14:43 +0530

    Update Buildimage.groovy

--- vars/Buildimage.groovy
+++ vars/Buildimage.groovy
@@ -1,6 +1,5 @@
- def call(dockeruser ,projectname ,tages)
-{
-    echo 'This area builds the code'
-    sh "docker build --no-cache -t ${dockeruser}/${projectname}:${tages} ."
-    echo "Successfully built the image"
+def call( dockeruser,  projectname,  tags) {
+  echo 'This area builds the code'
+  sh "docker build --no-cache -t ${dockeruser}/${projectname}:${tags} ."
+  echo "Successfully built the image"
 }
