2b28dddb7be619b2a7b048951dd18ec584f85076
abhayjt <ad.dubey23@gmail.com>
2025-06-18 16:11:15 +0530

    Update dockerpushlv.groovy

--- vars/dockerpushlv.groovy
+++ vars/dockerpushlv.groovy
@@ -1,1 +1,19 @@
 
+def call(String username, String projectName, String version) {
+    withCredentials([usernamePassword(
+        credentialsId: "dockerHubCred",
+        usernameVariable: "dockerHubUser",
+        passwordVariable: "dockerHubPass"
+    )]) {
+        echo "Logging into Docker Hub..."
+        sh 'echo $dockerHubPass | docker login -u $dockerHubUser --password-stdin'
+
+        echo "Tagging the image for Docker Hub..."
+        sh "docker tag ${username}/${projectName}:${version} ${dockerHubUser}/${projectName}:${version}"
+
+        echo "Pushing the image to Docker Hub..."
+        sh "docker push ${dockerHubUser}/${projectName}:${version}"
+
+        echo "Successfully pushed: ${dockerHubUser}/${projectName}:${version}"
+    }
+}
